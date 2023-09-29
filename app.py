862ecb8027a3e2f1a6f1e16dd5f08f677c54abe6
Prudhvish Narayanam <prudhvish11@gmail.com>
2023-09-29 22:05:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,3 @@ def predict_datapoint():
         print("after prediction")
 
         return render_template('home.html',results=results[0])
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
